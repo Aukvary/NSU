acd0d4bfff404c61f1c00a3babfc8a90fef62391
Aukvary <154656953+Aukvary@users.noreply.github.com>
2025-09-08 21:26:57 +0700

    tasks1

--- imperativ/f_sem/tasks1/task2.c
+++ imperativ/f_sem/tasks1/task2.c
@@ -7,14 +7,16 @@ int main(void) {
 
     if (x <= 0) {
         printf("%d", -1);
-        return;
+        return 0;
     } else if (y <= 0) {
         printf("%d", -2);
-        return;
+        return 0;
     } else if (z <= 0) {
         printf("%d", -3);
-        return;
+        return 0;
     }
 
     printf("%d\n", (x * y + x * z + y * z) * 2);
+
+    return 0;
 }
