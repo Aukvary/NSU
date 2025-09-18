318ca6d25c3ba2c31faa06e9e68f1b1d34272b44
Aukvary <154656953+Aukvary@users.noreply.github.com>
2025-09-18 19:33:39 +0700

    penis

--- imperativ/f_sem/tasks2/5.c
+++ imperativ/f_sem/tasks2/5.c
@@ -4,12 +4,25 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    char* s;
-    scanf("%s", &s);
-    
-    for (int i = 0; i * 8 < n; i++) {
-        for (int j = (i - 1) * 8; j < i * 8; j++) {
-            
+    char c;
+    int num;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%c", &c);
+
+        if (c != '1' && c != '0') {
+            i--;
+            continue;
+        }
+        
+        if (c == '1')
+            num += 1 << (i % 8);
+
+        if (i % 8 == 7) {
+            printf("%d ", num);
+            num = 0;
         }
     }
+
+    printf("%d", num);
 }
