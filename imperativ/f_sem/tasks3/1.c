13f0516877d37b4d0a652d936f95978ac1ccdbec
Aukvary <154656953+Aukvary@users.noreply.github.com>
2025-09-24 09:43:10 +0700

    jjj

---
+++ imperativ/f_sem/tasks3/1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    int buf[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &buf[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int count = 0;
+
+        for (int j = i + 1; j < n; j++) {
+            if (buf[i] > buf[j])
+                count += 1;
+        }
+        printf("%d ", count);
+    }
+
+    printf("0");
+
+    return 0;
+}
---
+++ imperativ/f_sem/tasks3/2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    int buf[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &buf[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    return 0;
+}
