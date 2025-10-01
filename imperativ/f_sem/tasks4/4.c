c5e02b8fc267ee324079eacb609baf6314bdf36e
Долгих Алексей Александрович <a.dolgikh3@main.nsu.ru>
2025-10-01 15:40:09 +0700

    sdf

--- imperativ/f_sem/tasks4/4.c
+++ imperativ/f_sem/tasks4/4.c
@@ -2,8 +2,8 @@
 
 int calcLetters(char *iStr, int *oLowerCnt, int *oUpperCnt, int *oDigitsCnt) {
     int i = 0;
-    for (; iStr[i] != '\0'; i++) {
-        if (iStr[i] <= '9' && iStr[i] >= 0) *oDigitsCnt += 1;
+    for (; iStr[i] != '\0' && iStr[i] != '\n'; i++) {
+        if (iStr[i] <= '9' && iStr[i] >= '0') *oDigitsCnt += 1;
         else if (iStr[i] <= 'Z' && iStr[i] >= 'A') *oUpperCnt += 1;
         else if (iStr[i] <= 'z' && iStr[i] >= 'a') *oLowerCnt += 1;
     }
@@ -19,9 +19,9 @@ int main() {
     int h = 0;
     int d = 0;
     int i = 0;
-    for (int c = l = h = d = i = 0; fscanf(in, "%s\n", s) != -1; i++, c = l = h = d = 0) {
+    for (int c = l = h = d = i = 0; fgets(s, 127, in); i++, c = l = h = d = 0) {
         c = calcLetters(s, &l, &h, &d);
-        printf("Line %d has %d chars: %d are letters (%d lower, %d upper), %d are digits.\n", i, c, l + h, l, h, d);
+        printf("Line %d has %d chars: %d are letters (%d lower, %d upper), %d are digits.\n", i + 1, c, l + h, l, h, d);
     }
 
     return 0;
---
+++ imperativ/f_sem/tasks4/5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define SIZE 1000000
+
+int main() {
+    FILE* in = fopen("input.txt", "r");
+    char buf[SIZE] = { 0 };
+    char ch = 0;
+    int bp = 0;
+    int h = 0;
+    int c = 0;
+    for (int i = 0; (ch = fgetc(in)) != EOF; i++) {
+        if (ch == '.' || ch == ',' || ch == ':' || ch == ';') {
+            if (c > 0)
+                printf("%d/%d %s\n", h, c, buf);
+
+            for (int j = 0; j < bp; j++)
+                buf[j] = 0;
+            h = 0;
+            c = 0;
+            bp = 0;
+            continue;
+        }
+        c++;
+        if (ch >= 'A' && 'Z' >= ch)
+            h++;
+        buf[bp++] = ch;
+    }
+
+    if (c > 0)
+        printf("%d/%d %s\n", h, c, buf);
+
+    return 0;
+}
