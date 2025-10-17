58c27ff8ec7bd4a6342b4e68c08ee9364babd108
Aukvary <154656953+Aukvary@users.noreply.github.com>
2025-10-18 05:07:59 +0700

    tasks6

--- ML/main.py
+++ ML/main.py
@@ -1,8 +1,7 @@
-a = 10
+f = lambda r, q, p, A: (((not r) or (not q)) and ((not r) or p) and (not A)) or (A and ((not p) or q) and r)
 
-match a:
-    case 10:
-        print(10)
-
-    case 10:
-        print(20)
+for r in range(2):
+    for q in range(2):
+        for p in range(2):
+            if not f(r, q, p, 1):
+                print('op')
--- imperativ/f_sem/tasks6/4.c
+++ imperativ/f_sem/tasks6/4.c
@@ -1,27 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
+typedef struct {
+    uint64_t code;
+    uint32_t l;
+} symb;
+
 int main() {
-    FILE* in = fopen("input.in", "rb");
+    FILE* in = fopen("input.txt", "rb");
     FILE* out = fopen("output.txt", "wb");
     uint32_t n;
 
     fread(&n, sizeof(uint32_t), 1, in);
 
+    symb* alf = malloc(n * sizeof(symb));
+    uint32_t l;
     uint8_t byte;
-    uint64_t res = 0;
 
-    while (fread(&byte, 1, 1, in) == 1) {
-        for (int i = 0; i < n; i++) {
-            if (fread(&byte, 1, 1, in) != 1)
-                break;
-
-            res <<= 1;
-            res += byte;
+    for (int i = 0; i < n; i++) {
+        fread(&l, sizeof(uint32_t), 1, in);
+        alf[i].l = l;
+        alf[i].code = 0;
+        for (int j = 0; j < l; j++) {
+            fread(&byte, sizeof(uint8_t), 1, in);
+            alf[i].code = alf[i].code << 1;
+            alf[i].code += byte == 1;
         }
-
-        fwrite(&res, 8)
     }
+    
+    uint32_t m;
+    fread(&m, sizeof(uint32_t), 1, in);
+    
+    uint8_t cur = 0;   
+    int bit_count = 0;
+    uint8_t* res = malloc(m * 8 + 8);
+    int len = 0;
+    
+    for (int i = 0; i < m; i++) {
+        uint16_t index;
+        fread(&index, sizeof(uint16_t), 1, in);
+        
+        for (int j = alf[index].l - 1; j >= 0; j--) {
+            uint8_t bit = (alf[index].code >> j) & 1;
+            cur |= (bit << bit_count);
+            bit_count++;
+            
+            if (bit_count == 8) {
+                res[len++] = cur;
+                cur = 0;
+                bit_count = 0;
+            }
+        }
+    }
+
+    if (bit_count > 0) {
+        res[len++] = cur;
+    }
+
+    fwrite(res, sizeof(uint8_t), len, out);
+
 
     return 0;
 }
