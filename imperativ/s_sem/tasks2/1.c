561704d5a28958805523159000fd5efc774b6d45
Aukvary <154656953+Aukvary@users.noreply.github.com>
2026-02-11 21:36:38 +0700

    tasks 2

---
+++ imperativ/s_sem/tasks2/1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(long long argc, char* argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "No parameters specified.");
+        return 13;
+    }
+
+    char* mode;
+    long long mod_num = LLONG_MAX;
+    char has_mod = 0;
+    long long num1;
+    long long num2;
+
+    for (long long i = 1; i < argc; i++) {
+        if (strcmp("-m", argv[i]) == 0) {
+            mod_num = atoll(argv[++i]);
+            has_mod = 1;
+            continue;
+        }
+
+        mode = argv[i];
+        num1 = atoll(argv[++i]);
+        num2 = atoll(argv[++i]);
+
+    }
+
+    switch (mode[0]){
+        case 'a':{
+            num1 += num2;
+            num1 %= mod_num;
+            break;
+        }
+        case 's':{
+            num1 -= num2;
+            num1 %= mod_num;
+            break;
+        }
+        case 'm':{
+            num1 *= num2;
+            num1 %= mod_num;
+            break;
+        }
+    }
+
+    if (num1 < 0 && has_mod) num1 += mod_num;
+
+    printf("%lld", num1);
+    
+    return 0;
+}
