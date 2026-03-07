c7b9cc45bce0cc55b9889cca4e4d8919775f8d3c
Aukvary <al3xpop123@gmail.com>
2026-03-07 20:15:47 +0700

    tasks 4

--- imperativ/s_sem/tasks4/4/myblas.c
+++ imperativ/s_sem/tasks4/4/myblas.c
@@ -1,8 +1,5 @@
 #include <stddef.h>
 
-enum CBLAS_ORDER { CblasRowMajor = 101, CblasColMajor = 102 };
-enum CBLAS_TRANSPOSE { CblasNoTrans = 111, CblasTrans = 112, CblasConjTrans = 113 };
-
 void cblas_dgemm(const int Order, const int TransA, const int TransB,
                  const int M, const int N, const int K,
                  const double alpha, const double *A, const int lda,
