e3f7bdf3f43be40905c3bef1414d339d8ee6d925
Aukvary <al3xpop123@gmail.com>
2026-03-11 23:27:51 +0700

    ml 6

--- imperativ/main.c
+++ imperativ/main.c
@@ -1,7 +1,43 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main() {
-    printf("%.20lf", 0.1 + 0.2);
 
-    return 0;
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+ 
+
+typedef struct ListNode* node;
+bool isPalindrome(struct ListNode* head) {
+    node cur = head;
+    node last = head;
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    while (cur != NULL) {
+        node tmp = cur->next;
+        cur->next = last;
+        last->next = tmp;
+        cur = tmp;
+        last = tmp->next;
+
+        while (last != NULL && last->next != NULL) {
+            last = last->next;
+        }
+    }
+
+    cur = head;
+
+    while (cur != NULL && cur->next != NULL) {
+        if (cur->val != cur->next->val) {
+            return false;
+        }
+
+        cur = cur->next->next;
+    }
+
+    return true;
 }
