f68f4813ffe670fbfe83e72a6756755294630d69
Aukvary <154656953+Aukvary@users.noreply.github.com>
2026-03-15 22:16:14 +0700

    tasks 18

--- imperativ/s_sem/tasks18/7/7.c
+++ imperativ/s_sem/tasks18/7/7.c
@@ -3,290 +3,173 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef struct Value Value;
+typedef enum { J_OBJECT, J_ARRAY, J_VALUE } NodeType;
 
-typedef struct ObjectField {
-    char* key;
-    Value* value;
-    struct ObjectField* next;
-} ObjectField;
-
-typedef struct Array {
-    Value** items;
+typedef struct Node {
+    NodeType type;
+    char *val;
+    struct Entry *entries;
     int count;
-    int capacity;
-} Array;
+} Node;
 
-struct Value {
-    int type;
-    union {
-        char* string;
-        Array* array;
-        ObjectField* object;
-        int number;
-        int boolean;
-        int null;
-    } data;
-};
+typedef struct Entry {
+    char *key;
+    Node *node;
+} Entry;
 
-enum {
-    TYPE_OBJECT,
-    TYPE_ARRAY,
-    TYPE_STRING,
-    TYPE_NUMBER,
-    TYPE_BOOLEAN,
-    TYPE_NULL
-};
-
-char* input;
-int pos;
-int input_len;
-
-char* read_string() {
-    pos++;
-    int start = pos;
-    while (input[pos] != '"') {
-        pos++;
-    }
-    int len = pos - start;
-    char* str = malloc(len + 1);
-    memcpy(str, input + start, len);
-    str[len] = '\0';
-    pos++;
-    return str;
-}
+char *buffer;
+int pos = 0;
 
 void skip_whitespace() {
-    while (pos < input_len && isspace(input[pos])) {
-        pos++;
-    }
+    while (buffer[pos] && isspace((unsigned char) buffer[pos])) pos++;
 }
 
-Value* parse_value();
-
-Value* parse_object() {
-    pos++;
+char *parse_string() {
     skip_whitespace();
-    ObjectField* head = NULL;
-    ObjectField* tail = NULL;
+    if (buffer[pos] == '"') pos++;
+    int start = pos;
+    while (buffer[pos] && buffer[pos] != '"') pos++;
+    int len = pos - start;
+    char *res = malloc(len + 3);
+    sprintf(res, "\"%.*s\"", len, buffer + start);
+    if (buffer[pos] == '"') pos++;
+    return res;
+}
 
-    if (input[pos] != '}') {
-        while (1) {
-            skip_whitespace();
-            char* key = read_string();
-            skip_whitespace();
+Node *parse_json();
+
+Node *parse_object() {
+    Node *node = calloc(1, sizeof(Node));
+    node->type = J_OBJECT;
+    pos++;
+    while (1) {
+        skip_whitespace();
+        if (buffer[pos] == '}') {
             pos++;
-            skip_whitespace();
-            Value* val = parse_value();
-
-            ObjectField* field = malloc(sizeof(ObjectField));
-            field->key = key;
-            field->value = val;
-            field->next = NULL;
-
-            if (!head) {
-                head = field;
-                tail = field;
-            } else {
-                tail->next = field;
-                tail = field;
-            }
-
-            skip_whitespace();
-            if (input[pos] == '}') break;
+            break;
+        }
+        char *key = parse_string();
+        skip_whitespace();
+        if (buffer[pos] == ':') pos++;
+        Node *val = parse_json();
+        node->entries = realloc(node->entries, sizeof(Entry) * (node->count + 1));
+        node->entries[node->count].key = key;
+        node->entries[node->count].node = val;
+        node->count++;
+        skip_whitespace();
+        if (buffer[pos] == ',') pos++;
+        else if (buffer[pos] == '}') {
             pos++;
-            skip_whitespace();
+            break;
         }
     }
-    pos++;
-
-    Value* obj_val = malloc(sizeof(Value));
-    obj_val->type = TYPE_OBJECT;
-    obj_val->data.object = head;
-
-    return obj_val;
+    return node;
 }
 
-Value* parse_array() {
+Node *parse_array() {
+    Node *node = calloc(1, sizeof(Node));
+    node->type = J_ARRAY;
     pos++;
-    skip_whitespace();
-    Array* arr = malloc(sizeof(Array));
-    arr->count = 0;
-    arr->capacity = 4;
-    arr->items = malloc(sizeof(Value*) * arr->capacity);
-
-    if (input[pos] != ']') {
-        while (1) {
-            skip_whitespace();
-            Value* val = parse_value();
-
-            if (arr->count >= arr->capacity) {
-                arr->capacity *= 2;
-                arr->items = realloc(arr->items, sizeof(Value*) * arr->capacity);
-            }
-            arr->items[arr->count++] = val;
-
-            skip_whitespace();
-            if (input[pos] == ']') break;
+    while (1) {
+        skip_whitespace();
+        if (buffer[pos] == ']') {
             pos++;
-            skip_whitespace();
+            break;
+        }
+        Node *val = parse_json();
+        node->entries = realloc(node->entries, sizeof(Entry) * (node->count + 1));
+        node->entries[node->count].key = NULL;
+        node->entries[node->count].node = val;
+        node->count++;
+        skip_whitespace();
+        if (buffer[pos] == ',') pos++;
+        else if (buffer[pos] == ']') {
+            pos++;
+            break;
         }
     }
-    pos++;
-
-    Value* arr_val = malloc(sizeof(Value));
-    arr_val->type = TYPE_ARRAY;
-    arr_val->data.array = arr;
-
-    return arr_val;
+    return node;
 }
 
-Value* parse_value() {
+Node *parse_simple_value() {
     skip_whitespace();
-    Value* val = malloc(sizeof(Value));
+    int start = pos;
+    while (buffer[pos] && !strchr(",}] \n\r\t", buffer[pos])) pos++;
+    int len = pos - start;
+    Node *node = calloc(1, sizeof(Node));
+    node->type = J_VALUE;
+    node->val = malloc(len + 1);
+    memcpy(node->val, buffer + start, len);
+    node->val[len] = '\0';
+    return node;
+}
 
-    if (input[pos] == '{') {
-        free(val);
-        return parse_object();
-    } else if (input[pos] == '[') {
-        free(val);
-        return parse_array();
-    } else if (input[pos] == '"') {
-        val->type = TYPE_STRING;
-        val->data.string = read_string();
-    } else if (strncmp(input + pos, "true", 4) == 0) {
-        val->type = TYPE_BOOLEAN;
-        val->data.boolean = 1;
-        pos += 4;
-    } else if (strncmp(input + pos, "false", 5) == 0) {
-        val->type = TYPE_BOOLEAN;
-        val->data.boolean = 0;
-        pos += 5;
-    } else if (strncmp(input + pos, "null", 4) == 0) {
-        val->type = TYPE_NULL;
-        val->data.null = 0;
-        pos += 4;
-    } else {
-        val->type = TYPE_NUMBER;
-        int start = pos;
-        if (input[pos] == '-') pos++;
-        while (isdigit(input[pos])) pos++;
-        int len = pos - start;
-        char* num_str = malloc(len + 1);
-        memcpy(num_str, input + start, len);
-        num_str[len] = '\0';
-        val->data.number = atoi(num_str);
-        free(num_str);
+Node *parse_json() {
+    skip_whitespace();
+    if (buffer[pos] == '{') return parse_object();
+    if (buffer[pos] == '[') return parse_array();
+    if (buffer[pos] == '"') {
+        Node *node = calloc(1, sizeof(Node));
+        node->type = J_VALUE;
+        node->val = parse_string();
+        return node;
     }
-
-    return val;
+    return parse_simple_value();
 }
 
-Value* parse_json() {
-    skip_whitespace();
-    return parse_value();
-}
+void process_queries(Node *root, FILE *f_in, FILE *f_out) {
+    int k;
+    while (fscanf(f_in, "%d\n", &k) == 1 && k != -1) {
+        Node *current = root;
 
-Value* find_by_path(Value* root, int k, char** steps) {
-    Value* current = root;
+        for (int i = 0; i < k; i++) {
+            char key[100] = {0};
+            fgets(key, sizeof(key), f_in);
 
-    for (int i = 0; i < k; i++) {
-        char* step = steps[i];
-
-        if (step[0] == '[') {
-            int index = atoi(step + 1);
-            if (current->type == TYPE_ARRAY) {
-                current = current->data.array->items[index];
-            }
-        } else {
-            if (current->type == TYPE_OBJECT) {
-                ObjectField* field = current->data.object;
-                while (field) {
-                    if (strcmp(field->key, step) == 0) {
-                        current = field->value;
+            if (key[0] == '\"') {
+                strrchr(key, '\"')[1] = '\0';
+                for (int j = 0; j < current->count; j++) {
+                    if (strcmp(current->entries[j].key, key) == 0) {
+                        current = current->entries[j].node;
                         break;
                     }
-                    field = field->next;
                 }
+            } else {
+                char str_idx[4] = {0};
+                sscanf(key, "[%[^]]]", str_idx);
+                int idx = atoi(str_idx);
+                current = current->entries[idx].node;
             }
         }
-    }
 
-    return current;
-}
-
-void print_value(Value* val) {
-    if (val->type == TYPE_OBJECT) {
-        printf("<object>\n");
-    } else if (val->type == TYPE_ARRAY) {
-        printf("<array>\n");
-    } else if (val->type == TYPE_STRING) {
-        printf("\"%s\"\n", val->data.string);
-    } else if (val->type == TYPE_NUMBER) {
-        printf("%d\n", val->data.number);
-    } else if (val->type == TYPE_BOOLEAN) {
-        printf(val->data.boolean ? "true\n" : "false\n");
-    } else if (val->type == TYPE_NULL) {
-        printf("null\n");
+        if (current->type == J_OBJECT) fprintf(f_out, "<object>\n");
+        else if (current->type == J_ARRAY) fprintf(f_out, "<array>\n");
+        else fprintf(f_out, "%s\n", current->val);
     }
 }
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    input = malloc(1048576);
-    input_len = 0;
+    FILE *f_in = fopen("input.txt", "r");
 
-    int c;
-    while ((c = getchar()) != EOF) {
-        input[input_len++] = c;
+    buffer = malloc(1024 * 1024);
+    int b_pos = 0;
+    char line[2048];
+
+    while (fgets(line, sizeof(line), f_in)) {
+        if (strstr(line, "<<<>>>")) break;
+        int len = strlen(line);
+        memcpy(buffer + b_pos, line, len);
+        b_pos += len;
     }
-    input[input_len] = '\0';
+    buffer[b_pos] = '\0';
 
-    char separator[] = "<<<>>>";
-    char* json_end = strstr(input, separator);
-    int json_len = json_end - input;
+    Node *root = parse_json();
 
-    char* json_input = malloc(json_len + 1);
-    memcpy(json_input, input, json_len);
-    json_input[json_len] = '\0';
+    FILE *f_out = fopen("output.txt", "w");
 
-    pos = 0;
-    input = json_input;
-    input_len = json_len;
-    Value* root = parse_json();
+    process_queries(root, f_in, f_out);
 
-    char* queries_start = json_end + strlen(separator);
-    char* query_pos = queries_start;
-
-    while (1) {
-        while (*query_pos == '\n' || *query_pos == '\r') query_pos++;
-
-        int k;
-        sscanf(query_pos, "%d", &k);
-        if (k == -1) break;
-
-        while (*query_pos != '\n' && *query_pos != '\r') query_pos++;
-        while (*query_pos == '\n' || *query_pos == '\r') query_pos++;
-
-        char** steps = malloc(sizeof(char*) * k);
-        for (int i = 0; i < k; i++) {
-            char* step_start = query_pos;
-            while (*query_pos != '\n' && *query_pos != '\r') query_pos++;
-            int step_len = query_pos - step_start;
-            steps[i] = malloc(step_len + 1);
-            memcpy(steps[i], step_start, step_len);
-            steps[i][step_len] = '\0';
-            while (*query_pos == '\n' || *query_pos == '\r') query_pos++;
-        }
-
-        Value* result = find_by_path(root, k, steps);
-        print_value(result);
-
-        for (int i = 0; i < k; i++) {
-            free(steps[i]);
-        }
-        free(steps);
-    }
-    
+    fclose(f_in);
+    fclose(f_out);
     return 0;
 }
