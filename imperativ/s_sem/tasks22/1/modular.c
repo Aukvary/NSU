011c1845ecd1bece3b80af7ce0ea5c169a3a8ebc
Aukvary <al3xpop123@gmail.com>
2026-04-12 22:17:04 +0700

    tasks 22

--- imperativ/s_sem/tasks22/1/modular.c
+++ imperativ/s_sem/tasks22/1/modular.c
@@ -2,6 +2,38 @@
 
 int MOD;
 
+static int inv_cache[1000000]; 
+static int last_MOD = 0;
+
+static int mod_inv(int a) {
+    if (MOD <= 1000000 && last_MOD == MOD && inv_cache[a] != 0) {
+        return inv_cache[a];
+    }
+    
+    int t = 0, newt = 1;
+    int r = MOD, newr = a;
+    
+    while (newr != 0) {
+        int q = r / newr;
+        int tmp = newt;
+        newt = t - q * newt;
+        t = tmp;
+        
+        tmp = newr;
+        newr = r - q * newr;
+        r = tmp;
+    }
+    
+    if (t < 0) t += MOD;
+    
+    if (MOD <= 1000000) {
+        last_MOD = MOD;
+        inv_cache[a] = t;
+    }
+    
+    return t;
+}
+
 int pnorm(int a) {
     int r = a % MOD;
     if (r < 0) r += MOD;
@@ -35,10 +67,6 @@ static int mod_pow(int a, int e) {
         e >>= 1;
     }
     return (int)result;
-}
-
-static int mod_inv(int a) {
-    return mod_pow(a, MOD - 2);
 }
 
 int pdiv(int a, int b) {
